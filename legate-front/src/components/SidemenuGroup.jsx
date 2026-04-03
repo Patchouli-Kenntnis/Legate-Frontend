6ba40873560fa5d87333241c181ea714ef33b984
Patchouli Kenntnis <hibernating_patchouli@protonmail.com>
2026-04-03 12:23:30 +0800

    Minor changes on sidebar style

--- legate-front/src/components/SidemenuGroup.jsx
+++ legate-front/src/components/SidemenuGroup.jsx
@@ -38,7 +38,7 @@ function SidemenuGrpConvBtn({ name }) {
     <li>
       <a
         href="#"
-        className="block rounded-lg px-4 py-2 text-sm font-medium text-gray-500 hover:bg-gray-100 hover:text-gray-700"
+        className="block rounded-lg px-4 py-2 text-sm font-medium text-gray-500  dark:hover:bg-gray-800 dark:hover:text-white dark:text-gray-400 hover:bg-gray-100 hover:text-gray-700"
       >
         {name}
       </a>
